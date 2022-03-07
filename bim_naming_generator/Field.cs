a732ffccdbc3d85e84de8e5b2546e964b4683cf3
Filip Kowalczyk <Filip.Kowalczyk@exyte-hargreaves.net>
2022-03-07 12:31:25 +0000

    initial commit

---
+++ bim_naming_generator/Field.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bim_naming_generator
+{
+    internal class Field
+    {
+        protected String content;
+        private int minChars, maxChars;
+
+        public Field(String content, int minChars, int maxChars)
+        {
+            this.content = content;
+            this.minChars = minChars;
+            this.maxChars = maxChars;
+        }
+
+        public Boolean IsValid()
+        {
+            return content.Length >= minChars || content.Length <= maxChars;
+        }
+
+        public String getDisplayString()
+        {
+            if (IsValid())
+            {
+                return content;
+            } else
+            {
+                return "?";
+            }
+        }
+    }
+}
---
+++ bim_naming_generator/MainForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace bim_naming_generator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRole = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.lblLevelsAndLocations = new System.Windows.Forms.Label();
+            this.lblVolumeOrSystem = new System.Windows.Forms.Label();
+            this.lblOriginator = new System.Windows.Forms.Label();
+            this.lblProjectCode = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblFileName.Location = new System.Drawing.Point(0, 402);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(800, 48);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "E21356-EHL-XX-XX-GU-XX-000001";
+            this.lblFileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblNumber.Location = new System.Drawing.Point(119, 294);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(265, 50);
+            this.lblNumber.TabIndex = 7;
+            this.lblNumber.Text = "Number";
+            this.lblNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 0;
+            // 
+            // lblRole
+            // 
+            this.lblRole.AutoSize = true;
+            this.lblRole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblRole.Location = new System.Drawing.Point(119, 245);
+            this.lblRole.Name = "lblRole";
+            this.lblRole.Size = new System.Drawing.Size(265, 49);
+            this.lblRole.TabIndex = 6;
+            this.lblRole.Text = "Role";
+            this.lblRole.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblType.Location = new System.Drawing.Point(119, 196);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(265, 49);
+            this.lblType.TabIndex = 5;
+            this.lblType.Text = "Type";
+            this.lblType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLevelsAndLocations
+            // 
+            this.lblLevelsAndLocations.AutoSize = true;
+            this.lblLevelsAndLocations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblLevelsAndLocations.Location = new System.Drawing.Point(119, 147);
+            this.lblLevelsAndLocations.Name = "lblLevelsAndLocations";
+            this.lblLevelsAndLocations.Size = new System.Drawing.Size(265, 49);
+            this.lblLevelsAndLocations.TabIndex = 4;
+            this.lblLevelsAndLocations.Text = "Levels and Locations";
+            this.lblLevelsAndLocations.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblVolumeOrSystem
+            // 
+            this.lblVolumeOrSystem.AutoSize = true;
+            this.lblVolumeOrSystem.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblVolumeOrSystem.Location = new System.Drawing.Point(119, 98);
+            this.lblVolumeOrSystem.Name = "lblVolumeOrSystem";
+            this.lblVolumeOrSystem.Size = new System.Drawing.Size(265, 49);
+            this.lblVolumeOrSystem.TabIndex = 3;
+            this.lblVolumeOrSystem.Text = "Volume or System";
+            this.lblVolumeOrSystem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOriginator
+            // 
+            this.lblOriginator.AutoSize = true;
+            this.lblOriginator.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblOriginator.Location = new System.Drawing.Point(119, 49);
+            this.lblOriginator.Name = "lblOriginator";
+            this.lblOriginator.Size = new System.Drawing.Size(265, 49);
+            this.lblOriginator.TabIndex = 2;
+            this.lblOriginator.Text = "Originator";
+            this.lblOriginator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblProjectCode
+            // 
+            this.lblProjectCode.AutoSize = true;
+            this.lblProjectCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblProjectCode.Location = new System.Drawing.Point(119, 0);
+            this.lblProjectCode.Name = "lblProjectCode";
+            this.lblProjectCode.Size = new System.Drawing.Size(265, 49);
+            this.lblProjectCode.TabIndex = 1;
+            this.lblProjectCode.Text = "Project Code";
+            this.lblProjectCode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Controls.Add(this.lblProjectCode, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblOriginator, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblVolumeOrSystem, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblLevelsAndLocations, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblType, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lblRole, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.lblNumber, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox3, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox4, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox5, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox6, 2, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 344);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Project 1",
+            "Project 2",
+            "Project 3",
+            "Project 4",
+            "Project 5"});
+            this.comboBox1.Location = new System.Drawing.Point(390, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(390, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(265, 24);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(390, 110);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(265, 24);
+            this.comboBox3.TabIndex = 10;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(390, 159);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(265, 24);
+            this.comboBox4.TabIndex = 11;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(390, 208);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(265, 24);
+            this.comboBox5.TabIndex = 12;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(390, 257);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(265, 24);
+            this.comboBox6.TabIndex = 13;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lblFileName);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRole;
+        private System.Windows.Forms.Label lblType;
+        private System.Windows.Forms.Label lblLevelsAndLocations;
+        private System.Windows.Forms.Label lblVolumeOrSystem;
+        private System.Windows.Forms.Label lblOriginator;
+        private System.Windows.Forms.Label lblProjectCode;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
+
